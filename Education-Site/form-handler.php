6ad14a05ec1945d5934fe0f475022d556e21b49f
anishajoshi6199 <anishajoshi6199@gmail.com>
2022-08-31 20:22:34 +0530

    ready to go

---
+++ Education-Site/form-handler.php
@@ -0,0 +1,31 @@
+<?php
+$name = $_POST['name'];
+$visiter_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+$email_from - 'anishajoshi6199@gmail.com';
+
+$email_subject - 'New Form Submission';
+
+$email_body = "User Name: $name.\n". :
+                "User Email: $visiter_email.\n".
+                  "subject: $subject.\n".
+                    "User Message: $message.\n";
+
+$to = 'anishajoshi6199@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visiter_email \r\n";
+
+
+mail($to, $email_subject, $email_body $headers);
+
+header("Location: contact.html");
+
+
+
+
+?>
